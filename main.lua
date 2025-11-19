5d981a3eacf30f55994e3c78f7838a62284ac832
walldmtd <walldmtd@proton.me>
2025-11-18 20:41:03 -0600

    feat: extract and cache previews from .blend and .blend1 files with `blender-thumbnailer`

--- main.lua
+++ main.lua
@@ -25,35 +25,13 @@ function M:preload(job)
 		return true
 	end
 
-	local cmd = M.with_limit()
-	if job.args.flatten then
-		cmd:arg("-flatten")
-	end
-	cmd:arg({ tostring(job.file.url), "-auto-orient", "-strip" })
+	local cmd = Command("blender-thumbnailer"):arg({ tostring(job.file.url), tostring(cache) })
 
-	local size = string.format("%dx%d>", rt.preview.max_width, rt.preview.max_height)
-	if rt.preview.image_filter == "nearest" then
-		cmd:arg({ "-sample", size })
-	elseif rt.preview.image_filter == "catmull-rom" then
-		cmd:arg({ "-filter", "catrom", "-thumbnail", size })
-	elseif rt.preview.image_filter == "lanczos3" then
-		cmd:arg({ "-filter", "lanczos", "-thumbnail", size })
-	elseif rt.preview.image_filter == "gaussian" then
-		cmd:arg({ "-filter", "gaussian", "-thumbnail", size })
-	else
-		cmd:arg({ "-filter", "triangle", "-thumbnail", size })
-	end
-
-	cmd:arg({ "-quality", rt.preview.image_quality })
-	if job.args.bg then
-		cmd:arg({ "-background", job.args.bg, "-alpha", "remove" })
-	end
-
-	local status, err = cmd:arg(string.format("JPG:%s", cache)):status()
+	local status, err = cmd:status()
 	if not status then
-		return true, Err("Failed to start `magick`, error: %s", err)
+		return true, Err("Failed to start `blender-thumbnailer`, error: %s", err)
 	elseif not status.success then
-		return false, Err("`magick` exited with error code: %s", status.code)
+		return false, Err("`blender-thumbnailer` exited with error code: %s", status.code)
 	else
 		return true
 	end
@@ -61,20 +39,6 @@ end
 
 function M:spot(job)
 	require("file"):spot(job)
-end
-
-function M.with_limit()
-	local cmd = Command("magick"):arg({ "-limit", "thread", 1 })
-	if rt.tasks.image_alloc > 0 then
-		cmd:arg({ "-limit", "memory", rt.tasks.image_alloc, "-limit", "disk", "1MiB" })
-	end
-	if rt.tasks.image_bound[1] > 0 then
-		cmd:arg({ "-limit", "width", rt.tasks.image_bound[1] })
-	end
-	if rt.tasks.image_bound[2] > 0 then
-		cmd:arg({ "-limit", "height", rt.tasks.image_bound[2] })
-	end
-	return cmd
 end
 
 return M
